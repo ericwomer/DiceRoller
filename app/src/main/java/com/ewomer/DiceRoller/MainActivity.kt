8785ba63a9afac0914b43a1d96839a432477b655
E. Womer <ewomer@hotmail.com>
2022-12-11 09:10:15 -0500

    Finished converting to Compose from Layout XML schema

--- app/src/main/java/com/ewomer/DiceRoller/MainActivity.kt
+++ app/src/main/java/com/ewomer/DiceRoller/MainActivity.kt
@@ -5,25 +5,34 @@ import android.os.Bundle
 import java.util.*
 
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.material.Text
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
-// widgets
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue // only if using var
 
-// project specific imports
+// Project specific imports
 import com.ewomer.diceroller.R
 
-// ide specific import
-// import com.itsaky.androidide.logsender.LogSender
+class MainActivity : AppCompatActivity() {
 
-public class MainActivity : AppCompatActivity() {
+    fun rollDice(): Int {
 
-    // private lateinit var binding: ActivityMainBinding
-
-    fun rollDice() {
         var randomInt = Random().nextInt(6) + 1
-        var drawableResource = when(randomInt) {
+        var drawableResource = when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -31,47 +40,47 @@ public class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-
-        // binding.diceImage.setImageResource(drawableResource)
+        return drawableResource
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        // Remove this line if you don't want AndroidIDE to show this app's logs
-        // LogSender.startLogging(this@MainActivity)
         super.onCreate(savedInstanceState)
-
-        setContent { MessageCard("Success!") }
-        /*
-        setContentView(R.layout.activity_main)
-        val rollButton : Button = findViewById(R.id.roll_button)
-        rollButton.text="Let's Roll"
-        Inflate and get instance of binding
-        binding = ActivityMainBinding.inflate(layoutInflater)
-
-        // set content view to binding's root
-        val view = binding.root
-        setContentView(view)
-
-        binding.rollButton.text = "Let's Roll"
-        binding.rollButton.setOnClickListener {
-            // Toast.makeText(this@MainActivity, "Button Clicked", Toast.LENGTH_SHORT).show()
-            rollDice()
-        }
-
-        
-        // Setup toolbar
-        // setSupportActionBar(binding.toolbar)
-        */
+        setContent { DiceRollerView() }
     }
-    
+
     @Composable
-    fun MessageCard(msg: String) {
-                Text(text = msg)
+    fun DiceRollerView() {
+        var diceImage by remember { mutableStateOf(R.drawable.empty_dice) }
+        /* DO Image */
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            item {
+                Image(
+                    painter = painterResource(id = diceImage),
+                    contentDescription = ""
+                )
+            }
+            item {
+                Button(
+                    onClick = { diceImage = rollDice() },
+                    elevation = ButtonDefaults.elevation(
+                        defaultElevation = 8.dp,
+                        pressedElevation = 2.dp
+                    )
+                ) {
+                    Text(text = "Lets Roll")
+                }
+            }
         }
+    }
 
     @Preview
     @Composable
     fun DiceRollerPreview() {
-        MessageCard(msg = "Success Again!")
+        DiceRollerView()
     }
+
 }
