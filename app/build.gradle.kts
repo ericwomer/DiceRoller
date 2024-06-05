7ed20b097c226e6539b6b334a8126e5940650287
EWomer <ewomer@hotmail.com>
2024-06-05 14:54:32 -0400

    Added Build Version Catalogs to the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application")
-    kotlin("android")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
 }
 
 android {
@@ -43,16 +43,17 @@ android {
 
 dependencies {
 
-    implementation ("androidx.core:core-ktx:1.9.0")
-    implementation ("androidx.appcompat:appcompat:1.5.1")
-    implementation ("com.google.android.material:material:1.7.0")
-    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
-    
-    implementation ("androidx.compose.compiler:compiler:1.3.2")
-    implementation("androidx.compose.ui:ui-tooling:1.3.2")
-    implementation ("androidx.compose.ui:ui-tooling-preview:1.3.2")
-    implementation ("androidx.compose.material:material:1.3.1")
-    implementation ("androidx.activity:activity-compose:1.6.1")
+    implementation (libs.androidx.core.ktx)
+    implementation (libs.androidx.appcompat)
+    implementation (libs.google.android.material)
+    implementation (libs.androidx.constraintlayout)
+
+    implementation (libs.androidx.compose.compiler)
+    implementation (libs.androidx.compose.ui.tooling)
+    implementation (libs.androidx.compose.ui.tooling.preview)
+    implementation (libs.androidx.compose.material)
+    implementation (libs.androidx.activity)
+
     // testImplementation "junit:junit:4.+"
     // androidTestImplementation "androidx.test.ext:junit:1.1.2"
     // androidTestImplementation "androidx.test.espresso:espresso-core:3.3.0"
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,4 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath ("com.android.tools.build:gradle:8.5.0-rc01")
-        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
-    }
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
 }
